9a533e76b9ab78aec19ed4774af55534ad424cf4
ImageFaded <33571471+ImageFaded@users.noreply.github.com>
2019-08-18 17:05:28 +0100

    Created Album Class To Store Information On Albums

---
+++ Album Organise - CL/Album.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Album_Organise___CL
+{
+    class Album
+    {
+        string ArtistName;
+        string AlbumName;
+        
+        public Album(string ArtistInput, string AlbumInput)
+        {
+            ArtistName = ArtistInput;
+            AlbumName = AlbumInput;
+        }        
+
+        public string GetArtistName()
+        {
+            return ArtistName;   
+        }
+
+        public string GetAlbumName()
+        {
+            return AlbumName;
+        }
+    }
+}
