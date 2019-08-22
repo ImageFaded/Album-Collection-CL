782d4a1f71919ae58a9561ac543bb41441aab02b
ImageFaded <33571471+ImageFaded@users.noreply.github.com>
2019-08-22 18:12:55 +0100

    Added In Different Types Of Albums

--- Album Organise - CL/Album.cs
+++ Album Organise - CL/Album.cs
@@ -5,11 +5,59 @@
     {
         //Variable for album name
         public string AlbumName;
+        public string AlbumFormat;
 
         //On creation, set input variables to object variables
         public Album(string AlbumName)
         {
             this.AlbumName = AlbumName;
+            AlbumFormat = null;
         }
     }
+
+    class EP : Album
+    {
+        public EP(string AlbumName) : base(AlbumName)
+        {
+            this.AlbumName = AlbumName;
+            AlbumFormat = "Ep";
+        }
+    }
+
+    class CD : Album
+    {
+        public CD(string AlbumName) : base(AlbumName)
+        {
+            this.AlbumName = AlbumName;
+            AlbumFormat = "CD";
+        }
+    }
+
+    class Vinyl : Album
+    {
+        public Vinyl(string AlbumName) : base(AlbumName)
+        {
+            this.AlbumName = AlbumName;
+            AlbumFormat = "Vinyl";
+        }
+    }
+
+    class Digital : Album
+    {
+        public Digital(string AlbumName) : base(AlbumName)
+        {
+            this.AlbumName = AlbumName;
+            AlbumFormat = "Digital";
+        }
+    }
+
+    class Cassette : Album
+    {
+        public Cassette(string AlbumName) : base(AlbumName)
+        {
+            this.AlbumName = AlbumName;
+            AlbumFormat = "Cassette";
+        }
+    }
+
 }
